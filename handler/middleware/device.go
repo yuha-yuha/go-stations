27c428547403af2beedae45997edb071a9b4a8f4
yuha-yuha <ishizushimadev@gmail.com>
2024-05-31 23:02:41 +0900

    DeviceKey構造体をmodel層に移した

--- handler/middleware/device.go
+++ handler/middleware/device.go
@@ -2,20 +2,21 @@ package middleware
 
 import (
 	"context"
+	"log"
 	"net/http"
 
+	"github.com/TechBowl-japan/go-stations/model"
 	"github.com/mileusna/useragent"
 )
 
 func Device(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 
-		type DeviceKey string
-
 		ua := useragent.Parse(r.UserAgent())
+		log.Println(r.UserAgent())
 		uaos := ua.OS
 
-		os := DeviceKey("OS")
+		os := model.DeviceKey("OS")
 
 		ctx := context.WithValue(r.Context(), os, uaos)
 
---
+++ model/device.go
@@ -0,0 +1,3 @@
+package model
+
+type DeviceKey string
